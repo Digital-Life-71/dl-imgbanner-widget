e8c61478621f7addb2f1775813e181f2e0cd7b7e
versusbassz <versus.post@gmail.com>
2014-01-31 23:55:59 +0400

    Many corrections of project

--- dl-imgbanner-widget.php
+++ dl-imgbanner-widget.php
@@ -29,45 +29,71 @@
 
 
 		public function widget ( $args , $instance ) {
-			if ( !$instance["stitle"] == '' ) $title = apply_filters( 'widget_title' , $instance['title'] );
+
+			$is_title = $instance["stitle"];
+
+			if ( $is_title ) {
+				$title = apply_filters( 'widget_title' , $instance['title'] );
+			}
+
+			$image_id = isset( $instance['image_id'] ) ? $instance['image_id'] : '';
+			$preview_image_src = $image_id ? $this->get_image_size( $image_id , 'full' ) : '';
 
 			extract( $args );
 
 			echo $before_widget;
-			echo $before_title;
-			echo $title;
-			echo $after_title;
-			echo '<img class="custom_media_image" src="' . $instance["image_uri"] . '" style="width:100%; display:inline-block" />';
+
+				if ( $is_title ) {
+					echo $before_title . $title . $after_title;
+				}
+
+				echo '<img class="custom_media_image" src="' . $preview_image_src . '" style="width:100%; display:inline-block" />';
+
 			echo $after_widget;
 		}
 
 
 		public function update ( $new_instance , $old_instance ) {
-			$instance = $old_instance;
+			$instance = array();
+			$instance['image_id'] = strip_tags( $new_instance['image_id'] );
 			$instance['title'] = strip_tags( $new_instance['title'] );
-			$instance['image_uri'] = strip_tags( $new_instance['image_uri'] );
 			$instance['stitle'] = strip_tags( $new_instance['stitle'] );
 			return $instance;
 		}
 
 
 		public function form ( $instance ) {
-			$image_uri = isset( $instance['image_uri'] ) ? $instance['image_uri'] : '';
+
+			$image_id = isset( $instance['image_id'] ) ? $instance['image_id'] : '';
+			$preview_image_src = $image_id ? $this->get_image_size( $image_id ) : '';
 			$title = isset( $instance['title'] ) ? $instance['title'] : '';
 			$stitle = isset( $instance['stitle'] ) ? $instance['stitle'] : '';
 			?>
 			<p>
-				<img class="custom_media_image" src="<?php echo $image_uri; ?>"
-				     style="width:100%; display:inline-block" />
+				<div style="text-align: center;">
+					<img
+						class="dl-image-preview"
+						src="<?php echo $preview_image_src; ?>"
+					/>
+				</div>
+
 				<input
 					type="hidden"
-					class="widefat custom_media_url"
-					name="<?php echo $this->get_field_name( 'image_uri' ); ?>"
-					id="<?php echo $this->get_field_id( 'image_uri' ); ?>"
-					value="<?php echo $image_uri; ?>"
+					class="widefat dl-image-id"
+					name="<?php echo $this->get_field_name( 'image_id' ); ?>"
+					id="<?php echo $this->get_field_id( 'image_id' ); ?>"
+					value="<?php echo $image_id; ?>"
 				/>
-				<a href="#" style="width: 100%; text-align: center;" class="button custom_media_upload">Загрузить/Выбрать
-					из библиотеки</a>
+				<a
+					href="#"
+					style="width: 100%;
+					text-align: center;"
+					class="button custom_media_upload"
+				    data-id="<?php echo $this->id; ?>"
+				>
+					Добавить медиафайл
+				</a>
+				<a href="javascript:void(0)" class="dl-clean-image">Очистить</a>
 			</p>
 			<p>
 				<label for="<?php echo $this->get_field_id( 'title' ); ?>">Заголовок:</label>
@@ -85,11 +111,22 @@
 					id="<?php echo $this->get_field_id( 'stitle' ); ?>"
 					name="<?php echo $this->get_field_name( 'stitle' ); ?>"
 					value="true" <?php if ( $stitle ) echo 'checked="checked"'; ?>
-					type="checkbox" checked
+					type="checkbox"
 				/>
 			</p>
 		<?php
 		}
+
+		public function get_image_size ( $attachment_id , $size = 'thumbnail' ) {
+			$thumb_info = wp_get_attachment_image_src( $attachment_id , $size );
+
+			if ( is_array( $thumb_info ) ) {
+				return $thumb_info['0'];
+			}
+
+			return false;
+		}
+
 	}
 
 
--- media_upload.js
+++ media_upload.js
@@ -1,19 +1,40 @@
-jQuery(function($){
-    $('.custom_media_upload').click(function(e) {
-        e.preventDefault();
-        var custom_uploader = wp.media({
-            title: 'Custom Title',
-            button: {
-                text: 'Custom Button Text',
-            },
-            multiple: false  // Set this to true to allow multiple files to be selected
-        })
-        .on('select', function() {
-            var attachment = custom_uploader.state().get('selection').first().toJSON();
-            $('.custom_media_image').attr('src', attachment.url);
-            $('.custom_media_url').val(attachment.url);
-            $('.custom_media_id').val(attachment.id);
-        })
-        .open();
-    });
-});
+
+jQuery( function ( $ ) {
+
+	$( document ).on( 'click' , '.custom_media_upload' , function ( e ) {
+		e.preventDefault();
+		var $target = $( e.target );
+
+		var widget_id = $target.attr('data-id');
+
+		var custom_uploader = wp.media( {
+			title    : 'Выберите изображение' ,
+			button   : {
+				text : 'Обновить'
+			} ,
+			multiple : false  // Set this to true to allow multiple files to be selected
+		})
+		.on( 'select' , function () {
+			var attachment = custom_uploader.state().get( 'selection' ).first().toJSON();
+			
+//			console.log( widget_id);
+			var $widget_box = $('.custom_media_upload[data-id="' + widget_id + '"]').parents( '.widget-content' );
+
+			$widget_box.find( '.dl-image-preview' ).attr( 'src' , attachment.sizes.thumbnail.url );
+			$widget_box.find( '.dl-image-id' ).val( attachment.id );
+		})
+		.open();
+	} );
+
+	$( document ).on( 'click' , '.dl-clean-image' , function ( e ) {
+		e.preventDefault();
+		var $widget_box = $( e.target ).parents( '.widget-content' );
+		
+//		console.log( $widget_box );
+
+		$widget_box.find('.dl-image-preview' ).attr( 'src' , '' );
+		$widget_box.find('.dl-image-id' ).val('');
+	} );
+} );
+
+
